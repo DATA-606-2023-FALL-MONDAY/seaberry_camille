50acda1fb8465154e4490f73ea4d20fdf3e122e3
camille-s <camille.seaberry@gmail.com>
2023-12-01 16:35:06 +0000

    Last tuning round for now

--- src/cue_tuning.sh
+++ src/cue_tuning.sh
@@ -4,7 +4,7 @@
 # chosing yolo_full, yolo_tile, and detr_full_frz
 # also transfer tiled to comb_full dataset
 rm -rf runs/detect/*tune*
-rm -rf runs/detect/*best
+# rm -rf runs/detect/*best
 
 python3 ./src/tune_yolo.py yolo_full_train \
     --dataset comb_full \
@@ -16,15 +16,25 @@ python3 ./src/tune_yolo.py yolo_full_train \
     --val 
 
 
-python3 ./src/tune_yolo.py detr_full_frz_train \
-    --dataset comb_full \
+# python3 ./src/tune_yolo.py detr_full_frz_train \
+#     --dataset comb_full \
+#     --model_type detr \
+#     --project_dir /home/paperspace/seaberry_camille \
+#     --tune_epochs 5 \
+#     --train_epochs 50 \
+#     --iterations 10 \
+#     --train \
+#     --val 
+
+python3 ./src/tune_yolo.py yolo_tile_train \
+    --dataset comb_tile \
     --project_dir /home/paperspace/seaberry_camille \
     --tune_epochs 5 \
     --train_epochs 50 \
     --iterations 10 \
     --train \
-    --val 
-
+    --val \
+    --label yolo_tile_notransfer
 
 python3 ./src/tune_yolo.py yolo_tile_train \
     --dataset comb_full \
